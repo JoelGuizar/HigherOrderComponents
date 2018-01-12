6881cb0c6277e44810db857b2b82c61cb6115a00
Joel Guizar <joelguizar@gmail.com>
2018-01-12 09:51:32 -0800

    require_auth redux connection, wrapper resources component with HOC

--- src/components/HOC/require_auth.js
+++ src/components/HOC/require_auth.js
@@ -1,12 +1,45 @@
 import React, {Component} from 'react';
-
+import {connect} from 'react-redux';
 //higher order component
 export default function(ComposedComponent) {
   class Authentication extends Component {
+    //ContextTypes are 'data' the whole application has access to
+    //this is how we access the Router component
+    //we have to define it, since Context can be easily abused
+    //instead of using props
+    //static = class Level property, not instance class
+    //now we have access to router by doing this below
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
+    //called before about to hit the DOM
+    componentWillMount(){
+      if (!this.props.authenticated){
+        this.context.router.push('/')
+      }
+    }
+
+    //called whenever about to have a new set of props or rerendered
+    //nextProps = next set of properties it will be handled with
+    componentWillUpdate(nextProps){
+    //if the next set of props still not authenticated/not auth'd any longer
+    //kick them out --- great to keep checking if they're still auth'd
+      if(!nextProps.authenticated){
+        this.context.router.push('/')
+      }
+    }
+
     render(){
+      //to make sure that any future props typed in, gets passed into here.
       return <ComposedComponent {...this.props}/>
     }
   }
 
-  return Authentication;
+  //makes the State into Props, now available through this.props
+  function mapStateToProps(state){
+    return {authenticated: state.autheticated}
+  }
+
+  return connect(mapStateToProps)(Authentication);
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, browserHistory } from 'react-router';
 
+import requireAuth from './components/HOC/require_auth'
 import App from './components/app';
 import Resources from './components/resources';
 import reducers from './reducers';
@@ -15,7 +16,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
       <Route path="/" component={App}/>
-      <Route path="resources" component={Resources}/>
+      //if every instance of Resource needs a requireAuthwrapper
+      //it is better to just wrap it in the Resource component,
+      //and then export the wrapped Resource component
+      <Route path="resources" component={requireAuth(Resources)}/>
     </Router>
   </Provider>
   , document.querySelector('.container'));
